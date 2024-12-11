5c3010d0a0e25c644ba2b562612a0162c4b66f13
Ihor Tymoshenko <igorz.tymoszenko@gmail.com>
2024-12-11 06:31:19 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Привет тебе, дитя. Назови своё имя?");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Каков свой возраст?");
+int userAge = int.Parse(Console.ReadLine());
+
+if (userAge >= 18)
+{
+    Console.WriteLine($"Добро пожаловать, {userName}! Присаживайся возле костра.");
+} 
+else 
+{
+    Console.WriteLine($"{userName}, приходи когда вырастешь! В данный момент тебе здесь не рады.");
+}
